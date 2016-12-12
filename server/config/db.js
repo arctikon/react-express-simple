bca69e79637caa1a1dd8ed20edf60a19f3175145
arctikon <arctikon@gmail.com>
2016-12-12 20:07:41 +0700

    make it better

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 //mongodb://localhost/products_db
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds145667.mlab.com:45667/heroku_8gf36s7b', function() {
+mongoose.connect('mongodb://arctikon:123gavno@ds145667.mlab.com:45667/heroku_8gf36s7b', function() {
 	console.log('mongo db connected');
 });
 
