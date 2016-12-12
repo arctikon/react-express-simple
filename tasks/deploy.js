bcd704eb4accc111d5acc14b1839f39f582fa0b7
arctikon <arctikon@gmail.com>
2016-12-12 16:30:46 +0700

    another

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -3,18 +3,16 @@
 //-----------------------------------------------
 
 const run = require('./run')
-const co = require('co')
 
 // Production model
 global.DEBUG = false
 
 function deploy() {
-  return co(function* () {
     run(require('./server'))
     yield run(require('./clean'))
     yield run(require('./bundle'))
     /* yield run(require('./publish')) */
-  })
+
 }
 
 module.exports =  deploy
